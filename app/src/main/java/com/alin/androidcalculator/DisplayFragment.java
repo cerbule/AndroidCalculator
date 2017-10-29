3574cdb13edb29c81d268ebdb1e49ad464af5aa1
cerbule <alindomuta@gmail.com>
2017-10-29 21:18:10 +0200

    introducing Butterknife

--- app/src/main/java/com/alin/androidcalculator/DisplayFragment.java
+++ app/src/main/java/com/alin/androidcalculator/DisplayFragment.java
@@ -6,6 +6,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 /**
@@ -13,17 +19,34 @@ import android.view.ViewGroup;
  */
 public class DisplayFragment extends Fragment {
 
+    @BindView(R.id.lbl_display)
+    TextView display;
+
+    @OnClick(R.id.imb_delete)
+    public void onDeleteShortClick(View v){
+
+    }
+
+    @OnClick(R.id.imb_delete)
+    public void onDeleteLongClick(View v){
+
+    }
 
     public DisplayFragment() {
         // Required empty public constructor
     }
 
+    public static DisplayFragment newInstance(){
+        return new DisplayFragment();
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_display, container, false);
+        View v = inflater.inflate(R.layout.fragment_display, container, false);
+        ButterKnife.bind(this, v);
+        return v;
     }
 
 }
